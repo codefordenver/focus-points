1c9a8ff4cba46a387c2a51f63dbfd8863d5b182b
Gray Smith <gray.smith00@gmail.com>
2019-02-25 20:10:59 -0700

    Graphql setup, not working

---
+++ backend/graphql.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: true,
+    credentials: true
+  }
+});
